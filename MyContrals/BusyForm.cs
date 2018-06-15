fe9979a2265798b78a71867d90146f0b991ab75a
hmpz <969243585@qq.com>
2018-06-15 08:45:52 +0800

    busyform修改了不显示一个没用的属性

--- MyContrals/BusyForm.cs
+++ MyContrals/BusyForm.cs
@@ -11,7 +11,7 @@ using System.Windows.Forms;
 namespace MyContrals
 {
 
-     public partial class BusyForm : Form
+     internal partial class BusyForm : Form
     {
 
         [DllImport("user32")]
